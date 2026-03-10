00e1244538da8b8b792cbf481278c9f428c7002b
Nghia <nghiabuivan1987@gmail.com>
2026-03-10 20:20:01 +0700

    java 21 again

--- src/main/java/com/example/simple/SimpleApplication.java
+++ src/main/java/com/example/simple/SimpleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimpleApplication {
 
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(SimpleApplication.class, args);
 	}
 
