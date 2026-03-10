c70fd3c41b05c6ac9707a2ee79d7e375554cc1b3
Nghia <nghiabuivan1987@gmail.com>
2026-03-10 21:31:51 +0700

    log some vars

--- src/main/java/com/example/simple/controllers/HomeController.java
+++ src/main/java/com/example/simple/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.simple.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +11,16 @@ public class HomeController {
     @GetMapping("/")
     public String home(
             HttpServletRequest request,
-            Model model
+            Model model,
+            @Value("${MYSQLHOST}") String mysqlHost,
+            @Value("${MYSQLPORT}") int mysqlPort,
+            @Value("${MYSQLUSER}") String mysqlUsername,
+            @Value("${MYSQLPASSWORD}") String mysqlPassword
     ) {
+        System.out.println(mysqlHost);
+        System.out.println(mysqlPort);
+        System.out.println(mysqlUsername);
+        System.out.println(mysqlPassword);
         var userLoggedIn = Utils.isUserLoggedIn(request);
         model.addAttribute("userLoggedIn", userLoggedIn);
         return "home";
